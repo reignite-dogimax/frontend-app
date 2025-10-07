02f2bd131fd1844218bad7df7cada5f1f3561954
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-10-07 17:59:01 -0500

    update: environment variables.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://dogimax-json-api.vercel.app',
   appointmentsEndpoint: '/citas',
   veterinariesEndpoint: '/veterinarias',
   petsEndpoint: '/mascotas',
