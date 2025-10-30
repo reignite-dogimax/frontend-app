be7c98b99043c9bdb6926e0de4400139b7cc18c7
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-10-30 00:15:56 -0500

    update: environments variables..

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  appointmentsEndpoint: '/citas',
-  veterinariesEndpoint: '/veterinarias',
+  apiUrl: 'http://localhost:8080',
+  appointmentsEndpoint: '/api/v1/appointments',
+  veterinariesEndpoint: '/api/v1/veterinarys',
   petsEndpoint: '/mascotas',
-  notificationsEndpoint: '/notificaciones',
-  userId: 1,
-  userName: 'Carlos'
+  notificationsEndpoint: '/notificaciones'
 };
